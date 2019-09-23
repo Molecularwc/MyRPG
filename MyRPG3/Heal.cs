ed85d44c231dfbbcc8f210f7e1f20074b9912b98
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-22 21:45:05 -0700

    Update Heal.cs
    name refactoring

--- MyRPG3/Heal.cs
+++ MyRPG3/Heal.cs
@@ -6,29 +6,29 @@ namespace MyRPG
     {
         public Heal()
         {
-            identifier = "Heal";
-            isOnSelf = true;
-            power = 180;
-            spellPwr = 0;
-            magicCost = 6;
+            Identifier = "Heal";
+            IsOnSelf = true;
+            Power = 180;
+            SpellPwr = 0;
+            MagicCost = 6;
         }
 
         public override int SpellCast(Character Caster)
         {
             Console.WriteLine("{0} casts heal,", Caster.Identifier);
-            Caster.CurrentMagic -= magicCost;
-            spellPwr = (power * Caster.Intelligence) / 5;
+            Caster.CurrentMagic -= MagicCost;
+            SpellPwr = (Power * Caster.Intelligence) / 5;
             if (Caster.CurrentMagic < 0)
             {
-                Caster.CurrentMagic += magicCost;
+                Caster.CurrentMagic += MagicCost;
                 Console.WriteLine("however {0} doesn't have enough magic points", Caster.Identifier);
-                power = 0;
+                Power = 0;
             }
             else if (Caster.CurrentMagic >= 0)
             {
-                Console.WriteLine("and heals {0}hp", spellPwr);
+                Console.WriteLine("and heals {0}hp", SpellPwr);
             }
-            return spellPwr;
+            return SpellPwr;
         }
     }
 }
