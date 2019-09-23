8ac384a1197e174bb348279a1a5ebcd58dc12375
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-22 21:05:35 -0700

    Update IncreaseMaxHP.cs
    minor refactoring

--- MyRPG3/IncreaseMaxHP.cs
+++ MyRPG3/IncreaseMaxHP.cs
@@ -13,18 +13,18 @@ namespace MyRPG
             _isUsed = false;
         }
 
-        public override int PotionUse(Character User)
+        public override int PotionUse(Character user)
         {
-            Console.WriteLine("{0} uses a Increase Max HP potion,", User.Identifier);
-            User.PotionQty = User.PotionQty - potionCost;
-            potency = User.MaxHealth * 10;
-            if (User.PotionQty < 0)
+            Console.WriteLine("{0} uses a Increase Max HP potion,", user.Identifier);
+            user.PotionQty = user.PotionQty - potionCost;
+            potency = user.MaxHealth * 10;
+            if (user.PotionQty < 0)
             {
-                User.PotionQty += potionCost;
-                Console.WriteLine("however {0} doesn't have enough potions to mix", User.Identifier);
+                user.PotionQty += potionCost;
+                Console.WriteLine("however {0} doesn't have enough potions to mix", user.Identifier);
                 potency = 0;
             }
-            else if (User.PotionQty >= 0)
+            else if (user.PotionQty >= 0)
             {
                 Console.WriteLine("and gains {0} additional HP!", potency);
             }
