08afbaf784c9767324acddb96a5b4f6366efcbbf
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-22 21:45:15 -0700

    Update Fireball.cs
    name refactoring

--- MyRPG3/Fireball.cs
+++ MyRPG3/Fireball.cs
@@ -6,29 +6,29 @@ namespace MyRPG
     {
         public Fireball()
         {
-            identifier = "Fireball";
-            multipleHits = true;
-            power = 10;
-            spellPwr = 0;
-            magicCost = 5;
+            Identifier = "Fireball";
+            MultipleHits = true;
+            Power = 10;
+            SpellPwr = 0;
+            MagicCost = 5;
         }
 
         public override int SpellCast(Character Caster)
         {
             Console.WriteLine("{0} casts fireball,", Caster.Identifier);
-            Caster.CurrentMagic -= magicCost;
-            spellPwr = (power * Caster.Intelligence) / 3;
+            Caster.CurrentMagic -= MagicCost;
+            SpellPwr = (Power * Caster.Intelligence) / 3;
             if (Caster.CurrentMagic < 0)
             {
-                Caster.CurrentMagic += magicCost;
+                Caster.CurrentMagic += MagicCost;
                 Console.WriteLine("however {0} doesn't have enough magic points", Caster.Identifier);
-                power = 0;
+                Power = 0;
             }
             else if (Caster.CurrentMagic >= 0)
             {
-                Console.WriteLine("and hits for {0}hp of fire damage", spellPwr);
+                Console.WriteLine("and hits for {0}hp of fire damage", SpellPwr);
             }
-            return spellPwr;
+            return SpellPwr;
         }
     }
 }
