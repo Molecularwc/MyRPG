f0398752c40982921fcf2fcb4b3822982308cc6f
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-22 21:44:24 -0700

    Update Invincibility.cs
    name refactoring, removed unused directives

--- MyRPG3/Invincibility.cs
+++ MyRPG3/Invincibility.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyRPG
 {
@@ -10,31 +6,31 @@ namespace MyRPG
     {
         public Invincibility()
         {
-            potionIdent = "Invincibility";
-            invincibility = true;
-            isOnSelf = true;
-            defenseMod = true;
-            potionCost = 3;
-            _isUsed = false;
+            PotionIdent = "Invincibility";
+            Invincibility = true;
+            IsOnSelf = true;
+            DefenseMod = true;
+            PotionCost = 3;
+            IsUsed = false;
         }
 
         public override int PotionUse(Character User)
         {
             Console.WriteLine("{0} uses an invincibility potion,", User.Identifier);
-            User.PotionQty = User.PotionQty - potionCost;
-            potency = User.Defense * 10;
+            User.PotionQty = User.PotionQty - PotionCost;
+            Potency = User.Defense * 10;
             if (User.PotionQty < 0)
             {
-                User.PotionQty += potionCost;
+                User.PotionQty += PotionCost;
                 Console.WriteLine("however {0} doesn't have enough potions to mix", User.Identifier);
-                potency = 0;
+                Potency = 0;
             }
             else if (User.PotionQty >= 0)
             {
-                Console.WriteLine("and gains {0} defense!", potency);
+                Console.WriteLine("and gains {0} defense!", Potency);
                 Console.WriteLine("{0} becomes invincible!", User.Identifier);
             }
-            return potency;
+            return Potency;
         }
     }
 }
