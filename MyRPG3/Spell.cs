425c0be99c702b80debeb5bf1afccecbb5cecfe4
Richard Salvaty <richardsalvaty@hotmail.com>
2019-09-22 21:28:33 -0700

    Update Spell.cs
    Refactored variables

--- MyRPG3/Spell.cs
+++ MyRPG3/Spell.cs
@@ -2,34 +2,34 @@
 {
     internal class Spell
     {
-        public bool fire, ice, lightning;
+        public bool Fire, Ice, Lightning;
 
         //These would be used if you want elemental defense and weakness
-        public string identifier;
+        public string Identifier;
 
-        public int magicCost;
+        public int MagicCost;
 
         //this is how much MP it costs to cast the spell
-        public bool multipleHits, singleTarget, isOnSelf;
+        public bool MultipleHits, SingleTarget, IsOnSelf;
 
-        public int power;//This is for the base potency of the spell
+        public int Power;//This is for the base potency of the spell
 
-        public int spellPwr; //This is the spell power mod
+        public int SpellPwr; //This is the spell power mod
 
         //These are to determine how to use the spell
         public Spell()
         {
-            multipleHits = false;
-            singleTarget = false;
-            isOnSelf = false;
-            fire = false;
-            ice = false;
-            lightning = false;
+            MultipleHits = false;
+            SingleTarget = false;
+            IsOnSelf = false;
+            Fire = false;
+            Ice = false;
+            Lightning = false;
         }
 
         public virtual int SpellCast(Character caster)
         {
-            return spellPwr;
+            return SpellPwr;
         }
     }
 }
